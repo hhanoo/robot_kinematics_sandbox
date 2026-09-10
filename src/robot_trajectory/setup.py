dbd61c71c528c995fd81828b3f16d659484c1b5e
hhanoo <woo980711@gmail.com>
2026-09-10 16:33:33 +0900

    [chore] Bump package versions to 1.0.0

--- src/robot_trajectory/setup.py
+++ src/robot_trajectory/setup.py
@@ -7,7 +7,7 @@ setup(
     # Package metadata
     # =========================================================
     name=package_name,
-    version="0.1.0",
+    version="1.0.0",
     # =========================================================
     # Package discovery
     # Automatically find all Python packages in the directory
